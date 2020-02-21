f455b2caaa7aadb40bd55adaa2c39a50b1ad779b
Kais <kais@blazeclan.com>
2020-02-21 17:21:45 +0100

    gitignore

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
-const app=express();
+const app = express();
 
-app.use('/graphql',graphqlHTTP({})
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
 
-);
-
-app.listen(4000,()=> {
-
-  console.log('now listening for requests on port 4000')
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
 });
